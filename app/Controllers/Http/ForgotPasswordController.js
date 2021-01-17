f2ef8940d3eade8f52328e11dbd49d2ffc26afac
Rogerio Coelho <rogerio@tidea.com.br>
2021-01-17 15:25:11 -0300

    testes

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const moment = require('moment')
+const crypto = require('crypto')
+
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+      await user.save()
+
+      await Mail.send(['emails.forgot_password'], { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` }, message => {
+        message
+          .to(user.email)
+          .from('rogerio@tidea.com.br', 'Rogerio | Tidea')
+          .subject('Recuperacao Senha')
+      })
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'Algo nao deu certo' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment().subtract('2', 'days').isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({ error: { message: 'Token expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    }
+    catch (err) {
+      console.log(err.message)
+      return response.status(err.status).send({ error: { message: 'Algo nao deu certo' } })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
---
+++ start/routes.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const { route } = require("@adonisjs/framework/src/Route/Manager")
+
+const Route = use('Route')
+
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+Route.get('/files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('/files', 'FileController.store')
+
+  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController').apiOnly()
+}).middleware(['auth'])
